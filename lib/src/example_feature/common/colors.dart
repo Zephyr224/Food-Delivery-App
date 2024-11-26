6c867d2749e4cc83739642f6f336d8b2fbd7bdf8
Karabo Shopane <zephyr.shopane@gmail.com>
2024-11-26 22:58:32 +0200

    Ui 80 percent done

---
+++ lib/src/example_feature/common/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// for the main texts
+final colorH1 = Colors.grey.shade900;
+final colorH2 = Colors.grey.shade700;
+final colorH3 = Colors.grey.shade500;
+final colorH4 = Colors.grey.shade400;
+final colorH5 = Colors.grey.shade300;
+
+// TextField colors
+final textFieldPrefixColor = Colors.grey.shade300;
+final textFieldSuffixColor = Colors.grey.shade300;
+final textFieldHintTextColor = Colors.grey.shade600;
+
+// SearchBar
+const searchBarBackgroundColor = Color(0xFFE5E5E5);
--- lib/src/example_feature/views/home_page.dart
+++ lib/src/example_feature/views/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/core/services/service_helpers/login_and_register_helpers.dart';
+import 'package:food_delivery_app/src/example_feature/common/colors.dart';
 import 'package:food_delivery_app/src/example_feature/common/constants.dart';
 import 'package:provider/provider.dart';
+
+import '../widgets/body_text_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -50,67 +53,126 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: customSafeArea),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SearchAnchor(
-                  builder: (context, controller) {
-                    return SearchBar(
-                      elevation: const WidgetStatePropertyAll(0),
-                      leading: const Icon(Icons.search),
-                      trailing: List.of(
-                        [
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.location_on_sharp,
-                              color: Colors.grey.shade600,
-                            ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: customSafeArea),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BodyTextWidget(
+                text: "Hello, Zephyr",
+                fontSize: 14,
+                color: colorH3,
+              ),
+              const SizedBox(height: 20),
+              const BodyTextWidget(
+                text: "Stay home, let",
+                fontSize: 30,
+              ),
+              const BodyTextWidget(
+                text: "the food come to you",
+                fontSize: 30,
+              ),
+              const SizedBox(height: 30),
+              SearchAnchor(
+                builder: (context, controller) {
+                  return SearchBar(
+                    elevation: const WidgetStatePropertyAll(0),
+                    leading: const Icon(Icons.search),
+                    trailing: List.of(
+                      [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.location_on_sharp,
+                            color: Colors.grey.shade600,
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                    hintText: "Search resturants and cuisine",
+                    hintStyle: WidgetStatePropertyAll(
+                        TextStyle(fontSize: 15, color: textFieldHintTextColor)),
+                    onTap: () {
+                      controller.openView();
+                    },
+                    onChanged: (value) {
+                      controller.openView();
+                      // rest of code?
+                    },
+                    backgroundColor:
+                        const WidgetStatePropertyAll(searchBarBackgroundColor),
+                  );
+                },
+                suggestionsBuilder: (context, controller) {
+                  // latest searched items
+                  return List<ListTile>.generate(
+                    5,
+                    (int index) {
+                      final String item = "Item $index";
+                      return ListTile(
+                        title: Text(item),
+                        onTap: () {
+                          setState(
+                            () {
+                              controller.closeView(item);
+                            },
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 40,
+                child: ListView.builder(
+                  itemCount: 5,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          side: index == 1
+                              ? const BorderSide(color: Colors.blueGrey)
+                              : BorderSide.none,
+                        ),
+                        child: Text(
+                          "Popular",
+                          style:
+                              TextStyle(color: index == 1 ? colorH3 : colorH4),
+                        ),
                       ),
-                      hintText: "Search resturants and cuisine",
-                      hintStyle: const WidgetStatePropertyAll(TextStyle(
-                        fontSize: 15,
-                      )),
-                      onTap: () {
-                        controller.openView();
-                      },
-                      onChanged: (value) {
-                        controller.openView();
-                        // rest of code?
-                      },
-                      backgroundColor:
-                          const WidgetStatePropertyAll(Colors.white38),
-                    );
-                  },
-                  suggestionsBuilder: (context, controller) {
-                    // latest searched items
-                    return List<ListTile>.generate(
-                      5,
-                      (int index) {
-                        final String item = "Item $index";
-                        return ListTile(
-                          title: Text(item),
-                          onTap: () {
-                            setState(
-                              () {
-                                controller.closeView(item);
-                              },
-                            );
-                          },
-                        );
-                      },
                     );
                   },
                 ),
-                SizedBox(height: 20),
-                // Text
-              ],
-            ),
+              ),
+              const SizedBox(height: 20),
+              BodyTextWidget(text: "Popular", fontSize: 30, ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return GridTile(
+                      header: const Padding(
+                        padding: EdgeInsets.only(top: 6),
+                        child: Icon(Icons.add_card),
+                      ),
+                      // footer: const Icon(Icons.remove_circle),
+                      child: Card(
+                        color: Colors.blueGrey.shade300,
+                        child: Text("Item $index"),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
           ),
         ),
       ),
